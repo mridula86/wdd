8e0b41987f94f3cb4c75af791ba9f2d05ec73353
mridula86 <134632126+mridula86@users.noreply.github.com>
2026-07-28 06:50:46 +0530

    Add files via upload

---
+++ Task 01-Salesletter/sales.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sales Letter</title>
+
+    <style>
+        body{
+            font-family: "Times New Roman", serif;
+            margin:40px;
+        }
+
+        h1{
+            font-size:45px;
+            margin-bottom:10px;
+        }
+
+        hr{
+            margin:20px 0;
+        }
+
+        .image{
+            text-align:center;
+            margin:20px;
+        }
+
+        .address{
+            line-height:1.6;
+        }
+
+        .letter{
+            font-size:20px;
+            line-height:1.8;
+            text-align:justify;
+        }
+
+        .signature{
+            text-align:right;
+            margin-top:40px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<h1>SALES LETTER</h1>
+
+<hr>
+
+<div class="image">
+    <img src="books.png" width="220" height="220" alt="Books">
+</div>
+
+<div class="address">
+<b>European Books Ltd</b><br>
+20<sup>th</sup> Johns Road<br>
+London 3W11 2Q3 ENGLAND<br>
+Tel: 01712169191 &nbsp;&nbsp; Fax: 0712160000
+</div>
+
+<br>
+
+December 24<sup>th</sup>, 2019
+
+<br><br>
+
+<i>
+IA LIBRARY INTERNATIONALE<br>
+24 Place Castelle<br>
+13006 Marseille<br>
+FRANCE
+</i>
+
+<br><br>
+
+<b>CUSTOMER</b>
+
+<div class="letter">
+
+<p>
+We have great pleasure in sending you a brochure about a new series of three
+textbooks <b>FLUENT ENGLISH</b> which provides interesting, challenging and
+provocative reading and discussion materials for students learning English at
+an upper-intermediate or advanced level.
+</p>
+
+<p>
+Inside the brochure, you will find a reply coupon to be completed and returned
+to European Books in London by June 30<sup>th</sup>. We will send you a specimen
+copy giving you a chance to examine <b>FLUENT ENGLISH</b> in more detail.
+</p>
+
+<p>
+If you have any queries concerning this new publication, please do not hesitate
+to contact us at the above address.
+</p>
+
+</div>
+
+<hr>
+
+<div class="signature">
+Yours sincerely,<br>
+<b>Peter Jones</b><br>
+Marketing<br>
+Manager<br><br>
+Encl: 1 brochure
+</div>
+
+</body>
+</html>
