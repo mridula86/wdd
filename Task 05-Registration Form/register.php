0aceb0f9a4899c2e5a631b1a5765b573dd863133
mridula86 <134632126+mridula86@users.noreply.github.com>
2026-07-28 06:54:50 +0530

    Add files via upload

---
+++ Task 05-Registration Form/register.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h2>Registration Details</h2>";
+
+echo "First Name: " . $_POST['first_name'] . "<br>";
+echo "Last Name: " . $_POST['last_name'] . "<br>";
+echo "Email: " . $_POST['email'] . "<br>";
+echo "Phone: " . $_POST['phone'] . "<br>";
+echo "Gender: " . $_POST['gender'] . "<br>";
+echo "Department: " . $_POST['department'] . "<br>";
+?>
